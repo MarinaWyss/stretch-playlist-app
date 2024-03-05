ae1b0ddb9c21b880d71fba6382039ea51d46a32e
MarinaWyss <marina.bennett.wyss@gmail.com>
2024-03-05 11:14:30 -0800

    first commit

---
+++ src/main/java/javaproject/marinalearnsjava/MarinaLearnsJavaApplication.java
@@ -0,0 +1,13 @@
+package javaproject.marinalearnsjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarinaLearnsJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarinaLearnsJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/javaproject/marinalearnsjava/MarinaLearnsJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package javaproject.marinalearnsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarinaLearnsJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
